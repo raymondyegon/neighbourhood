59f5c15fdeda11f4d538e4d98d00822defac4ba2
Raymond Yegon <raymondyegon17@gmail.com>
2019-05-30 12:06:21 +0300

    added the forms

---
+++ mtaani/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import UserProfile, Neighborhood, Business, Post, Comment
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('first_name', 'last_name', 'bio', 'neighborhood', 'email')
+
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['user', 'neighborhood']
+
+
+class PostForm(forms.ModelForm):
+    CHOICES = (('1', 'Amber',), ('2', 'Normal',))
+    type = forms.ChoiceField(widget=forms.Select, choices=CHOICES)
+
+    class Meta:
+        model = Post
+        fields = ('title', 'content', 'type')
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('comment',)
