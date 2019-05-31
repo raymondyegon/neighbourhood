e60d4624946624bd15defd648bf39d96af328edc
Raymond Yegon <raymondyegon17@gmail.com>
2019-05-31 08:05:28 +0300

    configured url

--- hood/urls.py
+++ hood/urls.py
@@ -22,11 +22,9 @@ from django_registration.backends.one_step.views import RegistrationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'',include('mtaani.urls')),
-    url(r'^accounts/register/',
-        RegistrationView.as_view(success_url='/accounts/login'),
-        name='django_registration_register'),
-    url(r'^accounts/', include('django_registration.backends.one_step.urls')),
+    url(r'', include('mtaani.urls')),
+    url(r'^accounts/register/', RegistrationView.as_view(success_url='/accounts/login'), name='django_registration_register'),
+    # url(r'^accounts/', include('django_registration.backends.one_step.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     # url(r'^logout/$', views.logout, {"next_page": '/'},name='logout')
 ]
